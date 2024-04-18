ee64d3ad3ccc1c47af656d651c406554820aa4aa
Leonel Sicaro <leonelsicaro@gmail.com>
2024-04-18 20:50:17 +0200

    mongo_uri connected

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
-const PORT= 3000
+const PORT= process.env.PORT || 5000 //si no encuentra port en process.env conectara al 5000
+
+app.get('/',(req,res)=>(res.send('hola')))
 
 app.listen(PORT,()=>{
     console.log(` esta corriendo en http://localhost:${PORT}`)
