29cbef2669bda6c1b9655a7640c69c8f6b59da37
Leonel Sicaro <leonelsicaro@gmail.com>
2024-04-18 21:07:05 +0200

    DATABASE CONNECTED

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require('dotenv').config()
+
+
 const express = require('express')
+const dbConnection = require('./config/config')
 const app = express()
 
 const PORT= process.env.PORT || 5000 //si no encuentra port en process.env conectara al 5000
@@ -9,3 +12,5 @@ app.get('/',(req,res)=>(res.send('hola')))
 app.listen(PORT,()=>{
     console.log(` esta corriendo en http://localhost:${PORT}`)
 })
+
+dbConnection()
