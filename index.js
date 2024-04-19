15f30c1ec9d263dc19ac697e5ce7447e18fccf07
Leonel Sicaro <leonelsicaro@gmail.com>
2024-04-19 14:48:57 +0200

    createController created

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const routes = require('./routes/products')
 const PORT= process.env.PORT || 5000 //si no encuentra port en process.env conectara al 5000
 
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 
 app.listen(PORT,()=>{
     console.log(` esta corriendo en http://localhost:${PORT}`)
--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const Products = require ('../models/Product')
 
-router.get('/',(req,res)=>(
-    res.send('lets go')
-)
-)
+const ProductController = require('../controllers/ProductController')
+
+//router.get('/',(req,res)=>(res.send('lets go')))
+router.post('/create',ProductController.create)
 
 module.exports = router
